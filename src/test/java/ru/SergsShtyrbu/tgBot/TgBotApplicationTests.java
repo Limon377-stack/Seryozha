e4ed8e59537c0db9b0c4cb1faae52d6985c2efae
Sergey <shturbu2003@bk.ru>
2025-07-11 15:50:34 +0300

    Практическое задание 2. Реализовал страницу Hello, world.

---
+++ src/test/java/ru/SergsShtyrbu/tgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.SergsShtyrbu.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
